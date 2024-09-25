e4e7250eb872c8cef2687d845348e3e8a9ae0a44
JuliMarteniuk <julia.marteniuk.epet20@gmail.com>
2024-09-25 08:27:47 -0300

    pantalla home preceptor, es muy parecida a los demas home pero tiene distinta informacion

---
+++ Morinalma/src/modules/preceptor/componentes/HomePreceptor.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import {StyleSheet} from 'react-native';
+
+const HomePreceptor = () => {
+    return(
+
+    <div>
+        <div style={StyleSheet.curso}>
+            <h3> 4°3 - Turno Vespertino</h3>
+            <h6> Ver información del curso</h6>
+        </div>
+        <div style={StyleSheet.curso}>
+            <h3> 5°3 - Turno Vespertino</h3>
+            <h6> Ver información del curso</h6>
+        </div>
+        <div style={StyleSheet.curso}>
+            <h3> 6°3 - Turno Vespertino</h3>
+            <h6> Ver información del curso</h6>
+        </div>
+    </div>    
+    
+    );  
+}
+
+const styles = StyleSheet.create({
+    curso:{
+        borderRadius: '20px',
+        with: '374px',
+        heigth: '96px',
+        backgroundColor: '#f6edfa',
+        color: '#000000',
+        border: '2px solid #833D8E',
+    },
+});
+
+
+export default HomePreceptor;
